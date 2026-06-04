75c25220b476bf4c6a6b8852040202dd02f034ed
Pulse <younass097@gmail.com>
2026-06-04 04:23:02 -0700

    Update script.js

--- functions/server/script.js
+++ functions/server/script.js
@@ -9,7 +9,7 @@ export async function onRequestGet(context) {
   }
 
   const scriptUrl =
-    `https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/games/${gameId}.lua`;
+    `https://raw.githubusercontent.com/BrickBotz/pulseware/main/games/${gameId}.lua`;
 
   const res = await fetch(scriptUrl);
 
